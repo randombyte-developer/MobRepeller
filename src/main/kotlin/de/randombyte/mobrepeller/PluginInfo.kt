c54aabd93b020e84c05b55baa7f8f42eedf2baac
randombyte <DSir1mio>
2016-03-09 20:40:23 +0100

    Changed plugin id because Sponge recommended it

--- src/main/kotlin/de/randombyte/mobrepeller/PluginInfo.kt
+++ src/main/kotlin/de/randombyte/mobrepeller/PluginInfo.kt
@@ -2,6 +2,6 @@ package de.randombyte.mobrepeller
 
 object PluginInfo {
     const val NAME = "MobRepeller"
-    const val ID = "MobRepeller"
+    const val ID = "mob_repeller"
     const val VERSION = "v0.1"
 }

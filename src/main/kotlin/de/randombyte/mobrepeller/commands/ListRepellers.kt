ba8362fd1fab03c32fc3323be680bbd23686c860
randombyte-developer <DSir1mio>
2016-03-12 21:19:29 +0100

    Reactivated command listRepellers

--- src/main/kotlin/de/randombyte/mobrepeller/commands/ListRepellers.kt
+++ src/main/kotlin/de/randombyte/mobrepeller/commands/ListRepellers.kt
@@ -1,6 +1,7 @@
 package de.randombyte.mobrepeller.commands
 
 import de.randombyte.mobrepeller.commands.PlayerCommunicator.error
+import de.randombyte.mobrepeller.commands.PlayerCommunicator.warn
 import de.randombyte.mobrepeller.State
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.CommandResult
@@ -11,24 +12,31 @@ import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.service.pagination.PaginationService
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.action.TextActions
+import org.spongepowered.api.text.format.TextColors.*
 
 class ListRepellers : CommandExecutor {
     override fun execute(src: CommandSource?, ctx: CommandContext?): CommandResult? {
-        when (src) {
+        val player = when (src) {
             null -> return CommandResult.empty()
             !is Player -> return src.error("Command must be executed by a player!")
+            else -> src
         }
 
-        val repellerTexts = State.repellers.map { repeller ->
-            Text.of("${repeller.key.position.toString()} with radius ${repeller.value}",
-                    TextActions.executeCallback { src ->
+        val repellersInWorld = State.repellers.filter { it.key.inExtent(player.location.extent) }
+        if (repellersInWorld.size == 0) return src.warn("No MobRepeller in this world!")
+
+        val repellerTexts = repellersInWorld.map { repeller ->
+            Text.builder()
+                    .append(Text.of(YELLOW, "Position: ${repeller.key.position.toString()} "))
+                    .append(Text.of(GREEN, "Radius: ${repeller.value.first}"))
+                    .onClick(TextActions.executeCallback { src ->
                         src.sendMessage(Text.of("Click on repeller at ${repeller.key.position.toInt()}"))
-                    })
+                    }).build()
         }
 
         val paginationServcie = Sponge.getServiceManager().provide(PaginationService::class.java).get()
         paginationServcie.builder()
-            .header(Text.of("Registered MobRepellers in this world"))
+            .header(Text.of(GREEN, "${repellersInWorld.size} MobRepeller(s) in this world: "))
             .contents(repellerTexts)
             .sendTo(src)
 
